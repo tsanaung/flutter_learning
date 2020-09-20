b500d11984b779539e2fcfa5474f23db22f1671a
Tsan Aung <myanmar@post.com>
2020-09-21 00:34:30 +0630

    Update main.dart

--- 01.hello_flutter/main.dart
+++ 01.hello_flutter/main.dart
@@ -6,44 +6,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Intro to Flutter",
+      title: "Hello Flutter!",
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: _MyHomePage(),
+      home: MyFlutterHome(),
     );
   }
 }
 
-class _MyHomePage extends StatelessWidget {
+class MyFlutterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello Flutter"),
-      ),
-      body: Container(
-        child: Row(
-          children: <Widget>[
-
-            IconButton(
-              icon: Icon(
-                Icons.favorite,
-                color: Colors.red,
-              ),
-              onPressed: null,
-              color: Colors.green,
-            ), //This is icon button with onPressed null
-
-            Text("Hello Flutter!"),
-
-            Icon(
-              Icons.audiotrack,
-              color: Colors.green,
-            ),
-            
-          ],
-        ),
+        title: Text("Hello Flutter!"),
       ),
     );
   }
